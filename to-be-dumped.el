2d415de234bdc6131703fbfd7bcab862248a7d92
Tom Harned <thomas.harned@gmail.com>
2021-11-08 09:51:00 -0500

    Update .emacs

---
+++ to-be-dumped.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(olivetti counsel-jq which-key web-mode use-package rg org-bullets magit lsp-pyright ido-vertical-mode ido-completing-read+ highlight-numbers highlight-escape-sequences flycheck flx-ido exec-path-from-shell evil-commentary evil-collection elpy eglot diminish dashboard counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ transient/history.el
@@ -0,0 +1,3 @@
+((magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-merge nil))
